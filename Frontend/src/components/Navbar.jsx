c16f39e97ad22711856348edb77798c4931eebd0
user.name:"Jeevan neupane <user.email:"neupanejeevan446@gmail.com>
2026-04-05 22:01:38 +0545

    navbar added for laptop

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+function Navbar() {
+  return (
+    <>
+      <div className=" h-[93px] bg-[#F2F2F2] w-full flex items-center px-[77px] justify-between">
+        {/* main navbar div */}
+
+        <div>
+          {/* left div for logo */}
+          <p className=" font-julius text-3xl text-[#31188E]">GHR</p>
+        </div>
+
+        <div className=" flex gap-15 items-center">{/* right div for other than logo */}
+        
+        <div className=" ">
+            {/* page links div */}
+          <ul className=" list-none flex font-albert gap-5 font-semibold" >
+            <li className=' cursor-pointer'>Home</li>
+           <li className=' cursor-pointer'>List Room</li>
+            <li className=' cursor-pointer'>Find Room</li>
+            <li className=' cursor-pointer'>Contact Us</li>
+          </ul>
+        </div>
+
+        <div className=" text-white font-semibold cursor-pointer hover:text-green-500 hover:duration-200  font-albert px-3 py-2 rounded-xl  bg-gradient-to-r from-[#1E1A81] to-[#080723]">
+            Login
+        </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Navbar;
